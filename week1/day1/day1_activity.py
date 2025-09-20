5d961c6c6c0d75313c71ed9665034abe8dd5a8e8
Mohamed Musharrif <musharrif@spil.com.au>
2025-09-20 16:56:30 +0530

    Day 01 of Data analytics

---
+++ week1/day1/day1_activity.py
@@ -0,0 +1,11 @@
+#Create 5 variables about yourself (first_name, city, fav_food, years_experience, likes_python).
+
+first_name = "Mohamed"
+city="Colombo"
+fav_food="shawarma"
+year_of_experience=3.5
+likes_python = True
+
+about_me = f"My name is {first_name}, I live in {city}. and I love {fav_food}"
+years_of_experience = f"\nI am a software engineer with {year_of_experience} years experience and If someone ask I love python ans the answer is {likes_python}"
+print(about_me + years_of_experience)
---
+++ week1/day1/day1_basics.py
@@ -0,0 +1,22 @@
+name = "Musharrif"
+age =25
+height = 5.8
+is_student = True
+
+print("Name",name)
+print("Age",age)
+print("Height",height)
+print("Are you a student ?",is_student)
+
+x = 10
+y=3.5
+z="Musharrif"
+a=[1,2,3,4]
+b={"name":"Musharrif","age":27}
+c={1,2,3,4}
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+print(type(c))
